0464bd3e549fcd913ed8a07991d2ccf48b72792a
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-11-06 20:14:47 +0000

    - Prepped for snapshot

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'software_repository';
-$app['version'] = '1.4.21';
+$app['version'] = '1.5.10';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
