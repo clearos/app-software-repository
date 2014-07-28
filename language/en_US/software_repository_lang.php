2ae2f5b834bc9f196e11764c91efd3034547a7ea
Marc Laporte <marc@laporte.name>
2014-07-28 16:40:42 -0400

    Fix some typos

--- language/en_US/software_repository_lang.php
+++ language/en_US/software_repository_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['software_repository_app_description'] = 'The Software Repository app prvoides a list of repositories available to the server.  Apps available in the Marketplace are dependant on which repositories are enabled.';
+$lang['software_repository_app_description'] = 'The Software Repository app provides a list of repositories available to the server.  Apps available in the Marketplace are dependent on which repositories are enabled.';
 $lang['software_repository_app_name'] = 'Software Repository';
 $lang['software_repository_number_of_packages'] = 'Packages';
 $lang['software_repository_name'] = 'Name';
 $lang['software_repository_busy'] = 'Software manager busy...';
 $lang['software_repository_repo_list'] = 'Repository List';
-$lang['software_repository_warning'] = 'You have enabled one or more repositories that may cause system stability/useability issues.  These repos are intended for developers, beta testers and advanced users only.  Please refer to the online documentation for more detailed information.';
+$lang['software_repository_warning'] = 'You have enabled one or more repositories that may cause system stability/usability issues.  These repos are intended for developers, beta testers and advanced users only.  Please refer to the online documentation for more detailed information.';
 $lang['software_repository_add_note:'] = 'The Marketplace app requires the following repository to be enabled:';
-$lang['software_repository_app_tooltip'] = 'Enabling and/or disabling repositories can have an adverse affect on the stability of the system.  Making changes to the default settings is recommended for advanced users only.';
+$lang['software_repository_app_tooltip'] = 'Enabling and/or disabling repositories can have an adverse effect on the stability of the system.  Making changes to the default settings is recommended for advanced users only.';
