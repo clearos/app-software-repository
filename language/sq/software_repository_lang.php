95c6d32c324ff08ca3d50eb47222f2154500d086
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 06:35:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/software_repository_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['software_repository_add_note:'] = 'The Marketplace app requires the following repository to be enabled:';
+$lang['software_repository_app_description'] = ' ';
+$lang['software_repository_app_name'] = 'Software Repository';
+$lang['software_repository_app_tooltip'] = ' ';
+$lang['software_repository_busy'] = 'Software manager busy...';
+$lang['software_repository_cli_only'] = 'CLI Only';
+$lang['software_repository_name'] = 'Name';
+$lang['software_repository_number_of_packages'] = 'Packages';
+$lang['software_repository_repo_list'] = 'Repository List';
+$lang['software_repository_warn_centos_disable'] = 'Consider disabling "clearos-centos" repository - this server has access to verified software updates through a paid service or first 30 days of a Community install.';
+$lang['software_repository_warn_centos_enable'] = 'Consider enabling the "clearos-centos" repository to keep your system up to date.';
+$lang['software_repository_warn_centos_updates_disable'] = 'Recommend disabling "clearos-centos-updates" repository.';
+$lang['software_repository_warn_centos_updates_enable'] = 'Consider enabling the "clearos-centos-updates" repository to keep your system up to date.';
+$lang['software_repository_warn_epel_duplicate'] = 'Consider disabling "clearos-epel" repository - this server has access to verified EPEL updates through a paid service or first 30 days of a Community install.';
+$lang['software_repository_warn_epel_is_enabled'] = '  ';  
+$lang['software_repository_warn_fast_updates_enable'] = 'Consider enabling "clearos-fast-updates" - this repository is used to quickly push out updates deemed critical to the security of your system.';
+$lang['software_repository_warn_test_repo'] = ' ';
+$lang['software_repository_warn_updates_disable'] = 'Consider disabling "clearos-updates" repository - this server has access to verified software updates through a paid service or first 30 days of a Community install.';
+$lang['software_repository_warn_updates_enable'] = 'Consider enabling "clearos-updates" repository... it provides software updates to fix bugs, add features and in some cases, patch critical security vunerabilities.';
+$lang['software_repository_warning'] = 'The following recommendations/warnings have been identified:';
+$lang['software_repository_updates_warning_event'] = 'Software repository configuration warning(s)';
